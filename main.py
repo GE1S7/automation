cf58f19a6af7895b1ce8e15ba17412a1493d1d8a
ge1s7 <kacperkalinowski@protonmail.com>
2024-12-12 20:52:40 +0100

    logs in with dft pass; checks autoupdate; init ssid/key setup

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # locate elements within a document
 from selenium.webdriver.common.by import By
+# sleep
+import time
+# access to environmental variables
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ADMIN_PASS=os.getenv("dft_pass")
+
 
 driver = webdriver.Chrome()
 driver.get("http://192.168.8.1/html/quicksetup.html")
@@ -11,4 +20,26 @@ driver.get("http://192.168.8.1/html/quicksetup.html")
 if "Mobile Broadband" in driver.title:
     print("I'm in.")
 
+login_box = driver.find_element(By.CLASS_NAME, "input")
+login_box.clear()
+#login_box.click()
+login_box.send_keys(ADMIN_PASS)
+login_box.send_keys(Keys.RETURN)
+time.sleep(0.2)
 
+au_box = driver.find_element(By.ID, "auto_update")
+au_box.click()
+
+next1 = driver.find_element(By.CLASS_NAME, "button_wrapper")
+next1.click()
+
+set_ssid = driver.find_element(By.ID, ssid_wifiName)
+set_ssid.send_keys(new_ssid)
+
+
+
+
+time.sleep(29999999)
+
+
+#<input type="password" autocomplete="off" class="input" id="password" maxlength="32" tabindex="1" style="" data-protonpass-base-css="{&quot;padding-right&quot;:&quot;&quot;}">
