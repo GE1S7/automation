78c86b54ea3eeb2d058872dcf81a9567f512c913
ge1s7 <kacperkalinowski@protonmail.com>
2024-12-13 17:10:05 +0100

    select encr type for w1&w2

--- add_set.py
+++ add_set.py
@@ -22,6 +22,15 @@ login_box.send_keys(Keys.RETURN)
 time.sleep(0.2)
 
 driver.get("http://192.168.8.1/html/wlanbasicsettings.html")
+
+select_element = driver.find_element(By.ID, "ssid_authentication")
+select = Select(select_element)
+select.select_by_value(ENCR_TYPE)
+apply_btn = driver.find_element(By.ID, "apply_button")
+apply_btn.click()
+time.sleep(5)
+
+
 editw2 = driver.find_element(By.XPATH, '//*[@id="ssid_list"]/tbody/tr[4]/td[5]/span')
 editw2.click()
 
@@ -31,9 +40,7 @@ w2_ssid_name.send_keys(W2_SSID)
 
 select_element = driver.find_element(By.ID, "ssid_authentication")
 select = Select(select_element)
-selected_option_list = select.all_selected_options
-time.sleep(0.2)
-print(select.options)
+#time.sleep(0.2)
 select.select_by_value(ENCR_TYPE)
 
 w2_key = driver.find_element(By.ID, "ssid_wpa_key")
@@ -43,7 +50,7 @@ w2_key.send_keys(W2_PASS)
 apply_btn = driver.find_element(By.ID, "apply_button")
 apply_btn.click()
 
-time.sleep(234234)
+time.sleep(2)
                                 
 
 
